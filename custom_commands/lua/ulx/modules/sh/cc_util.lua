0b78f08541f0bdc5ad8aa84887e94a976e211b51
TheFulDeep <zlobindd@mail.ru>
2019-01-27 11:48:10 +0300

    imagine fix

--- custom_commands/lua/ulx/modules/sh/cc_util.lua
+++ custom_commands/lua/ulx/modules/sh/cc_util.lua
@@ -2749,6 +2749,11 @@ if SERVER then
 		--PrintTable(NewMapTable)
 		NextMapTable = NewMapTable
 	end)
+
+
+	if game.GetMap():find("imagine") then
+		for k,v in pairs(ents.FindByClass("gmod_track_autodrive_plate")) do if v.PlateType == 760 then v:Remove() end end
+	end
 end
 --ply.InMetrostroiTrain
 --[[============================= УДАЛЕНИЕ НЕНУЖНЫХ ВКЛАДОК ИЗ SPAWNMENU ==========================]]
