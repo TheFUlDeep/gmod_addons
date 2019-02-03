ff81f3e51a055f4c9b7f047f027b58412072b60c
TheFulDeep <zlobindd@mail.ru>
2019-02-03 17:14:16 +0300

    persostant

--- sync/lua/autorun/sv_sync_trains.lua
+++ sync/lua/autorun/sv_sync_trains.lua
@@ -104,6 +104,7 @@ local function GetSyncedTrains(arg)
 				v1:SetModel(v.model)
 				v1:SetMoveType(MOVETYPE_NONE)
 				v1:SetMoveType(MOVETYPE_FLY)					-- установится ли?
+				v1:SetPersistent(true)
 				v1:SetPos(v.pos + Vector(200,0,0))
 				v1:SetAngles(v.ang)
 				--print(v.pos)
