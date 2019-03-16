2f85fa5a51680f26f6cdf94fae32ae2695a76608
TheFulDeep <zlobindd@mail.ru>
2019-03-16 11:48:33 +0300

    stringfund hotfix

--- string_tools/lua/autorun/stringfind.lua
+++ string_tools/lua/autorun/stringfind.lua
@@ -6,12 +6,12 @@ function stringfind(where, what, lowerr, startpos, endpos)
 	elseif string.len(what) > string.len(where) then --[[print("[STRINGFIND EXEPTION] string what you want to find bigger than string where you want to find it")]] Exeption = true 
 	end
 	if startpos and type(startpos) ~= "number" then startpos = tonumber(startpos) end
-	if endpos and type(startpos) ~= "number" then endpos = tonumber(endpos) end
+	if endpos and type(endpos) ~= "number" then endpos = tonumber(endpos) end
 	local strlen1 = string.len(where)
 	local strlen2 = string.len(what)
 	if not startpos or startpos == 0 then startpos = 1 end
 	if startpos < 1 then startpos = strlen1 + startpos + 1 end
-	if not endpos or endpos == 0 then endpos = strlen1	end
+	if not endpos or endpos == 0 then endpos = strlen1 end
 	if endpos < 1 then endpos = strlen1 + endpos + 1 end
 	if endpos > strlen1 then --[[print("[STRINGFIND EXEPTION] end position bigger then source string (source string size = "..#where..")")]] Exeption = true end
 	if endpos < startpos then --[[print("[STRINGFIND EXEPTION] end position smaller then start position")]] Exeption = true end
