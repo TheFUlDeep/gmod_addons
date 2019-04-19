7a6ca6842e652f88b65310aac91eccd67f44c22f
TheFulDeep <zlobindd@mail.ru>
2019-04-19 10:16:00 +0300

    thefuldeep's autorun and connect_spawn msg

---
+++ connect_spawn_message/lua/thefuldeep_autorun/connect_spawn_message/sh_connect_spawn_message.lua
@@ -0,0 +1,34 @@
+if SERVER then
+	util.AddNetworkString("tfd_spawnmsg")
+	util.AddNetworkString("tfd_connectmsg")
+	
+	hook.Add("PlayerInitialSpawn", "tfd_spawnmsg",function(ply)
+		timer.Simple(1,function()
+			if not IsValid(ply) then return end
+			net.Start("tfd_spawnmsg")
+				net.WriteString(ply:Nick())
+				net.WriteColor(team.GetColor(ply:Team()))
+			net.Broadcast()
+		end)
+	end)
+	
+	hook.Add("PlayerConnect", "tfd_connectmsg",function(nick)
+		net.Start("tfd_connectmsg")
+			net.WriteString(nick)
+		net.Broadcast()
+	end)
+end
+
+
+if CLIENT then
+	net.Receive("tfd_connectmsg",function()
+		local nick = net.ReadString()
+		chat.AddText(color_white,'Игрок "'..nick..'"', Color(255,255,0)," присоединяется",color_white,".")
+	end)
+	
+	net.Receive("tfd_spawnmsg",function()
+		local nick = net.ReadString()
+		local color = net.ReadColor()
+		chat.AddText(color_white,"Игрок ",'"',color,nick,color_white,'"',Color(150,255,0)," заспавнился",color_white,".")
+	end)
+end
