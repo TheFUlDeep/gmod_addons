88453b584139dc64e892ea8da5fef9a86410991a
TheFulDeep <zlobindd@mail.ru>
2019-04-19 13:05:53 +0300

    thefuldeep's autorun cl sv and sh update

--- thefuldeep_autorun_init/lua/autorun/thefuldeep_autorun_init.lua
+++ thefuldeep_autorun_init/lua/autorun/thefuldeep_autorun_init.lua
@@ -1,16 +1,38 @@
 local function InitAutorun(path,param)
 	--if not file.Exists(string.sub(path,1,-2),"LUA") then return end
 	local files, directories = file.Find( path.."*", "LUA" )
-	--PrintTable(files)
 	if files then
-		for k,v in pairs(files) do
-			--local content = file.Read(path..v, "LUA")
-			--if not content or content == "" then continue end
-			print("including "..path..v.."by TheFulDeep's autorun")
-			include(path..v)
-			if SERVER and (param == "sh" or param == "cl") then AddCSLuaFile(path..v) print("added "..path..v.." to download to clients") end
+		if SERVER then
+			for k,v in pairs(files) do
+				local content = file.Read(path..v,"LUA")
+				if content:find("%a") then
+					continue
+				else
+					files[k] = nil print("File "..path..v.." is empty. It will not be included.") 
+				end
+			end
+			if param == "sh" or param == "sv" then
+				for k,v in pairs(files) do
+					print("including "..path..v.." by TheFulDeep's autorun")
+					include(path..v) 
+				end
+			end
+			if param == "sh" or param == "cl" then
+				for k,v in pairs(files) do
+					AddCSLuaFile(path..v)
+					print("added "..path..v.." to download to clients")
+				end
+			end
+		end
+		
+		if CLIENT and (param == "sh" or param == "cl") then
+			for k,v in pairs(files) do
+				include(path..v)
+				print("included "..path..v.." by TheFulDeep's autorun")
+			end
 		end
 	end
+	
 	if directories then 
 		for k,v in pairs(directories) do
 			InitAutorun(path..v.."/",param)
