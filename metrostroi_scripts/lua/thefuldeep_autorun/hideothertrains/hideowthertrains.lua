f5f1716627b13f7329a2d4d7460ab0c5fad3a9dd
TheFulDeep <zlobindd@mail.ru>
2019-05-30 00:50:17 +0300

    hideothertrains low performance notification

--- metrostroi_scripts/lua/thefuldeep_autorun/hideothertrains/hideowthertrains.lua
+++ metrostroi_scripts/lua/thefuldeep_autorun/hideothertrains/hideowthertrains.lua
@@ -4,15 +4,15 @@ CreateClientConVar("hideothertrains", "0", true, false, "" )
 
 CreateClientConVar("hidealltrains","0",true,false,"")
 
-local fps
-local PrevFPS
-local FPSLimit = 15
-timer.Create("RecomendHideTrains",20,0,function()
-	if not system.HasFocus() then return end
-	PrevFPS = fps
-	fps = 1 / RealFrameTime()
-	if PrevFPS and PrevFPS < FPSLimit and fps < FPSLimit then
-		timer.Remove("RecomendHideTrains")
+
+local FPSLimit = 20
+local TimeLimit = 10 * FPSLimit
+local i = 0
+hook.Add("Think","RecomendHideTrains",function()
+	local fps = 1 / RealFrameTime()
+	if fps < FPSLimit then i = i + 1 else i = 0 end
+	if i > TimeLimit then
+		hook.Remove("Think","RecomendHideTrains")
 		chat.AddText(Color(255,0,0,200),"Обнаружена низкая производительность. Для повышения fps попробуйте консольные команды hideothertrains 1 или hidealltrains 1. А также убедитесь, что режим съемки выключен!")
 	end
 end)
