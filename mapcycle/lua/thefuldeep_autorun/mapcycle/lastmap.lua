9c16d469652b10248dbbef9c0578c89465e8df4e
TheFulDeep <zlobindd@mail.ru>
2019-07-12 03:36:02 +0300

    hotfix

--- mapcycle/lua/thefuldeep_autorun/mapcycle/lastmap.lua
+++ mapcycle/lua/thefuldeep_autorun/mapcycle/lastmap.lua
@@ -1,9 +1,10 @@
 --[[============================= ЗАГРУЗКА ПОСЛЕДНЕЙ КАРТЫ сохранение последней карты прописано в mapcycle ==========================]]
 if SERVER then
 timer.Create("CheckLastMap",1,0,function()
+	if not THEFULDEEP or not THEFULDEEP.MAP then return end
 	if file.Exists( "lastmap.txt", "DATA" ) then
 		local lastmap = file.Read( "lastmap.txt", "DATA" )
-		if lastmap and THEFULDEEP and THEFULDEEP.MAP and THEFULDEEP.MAP ~= lastmap then 
+		if lastmap and THEFULDEEP.MAP ~= lastmap then 
 			game.ConsoleCommand("changelevel "..lastmap.."\n") 		
 			return
 		end
