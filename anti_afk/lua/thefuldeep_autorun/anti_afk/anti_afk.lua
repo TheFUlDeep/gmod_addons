1acc301b1e9c2df42e39785c2b845a724a577712
TheFulDeep <zlobindd@mail.ru>
2019-07-30 21:16:20 +0300

    antiafk

---
+++ anti_afk/lua/thefuldeep_autorun/anti_afk/anti_afk.lua
@@ -0,0 +1,68 @@
+if CLIENT then return end
+
+CreateConVar("antiafk",300,{FCVAR_NEVER_AS_STRING,FCVAR_ARCHIVE})
+
+local AntiAfkTimeConVar = GetConVar("antiafk")
+
+timer.Create("AntiAfk",10,0,function()
+	local AntiAfkTime = AntiAfkTimeConVar:GetInt()
+	for n,ply in pairs(player.GetHumans()) do
+		if not IsValid(ply) then continue end
+		if not ply.AntiAfk then ply.AntiAfk = {} end
+		local Pos = ply:GetPos()
+		--local Ang = ply:GetAngles() --ply:EyeAngles()
+		if not ply.AntiAfk.Pos or ply.AntiAfk.Pos ~= Pos 
+		--or not ply.AntiAfk.Ang or ply.AntiAfk.Ang ~= Ang 
+		then
+			ply.AntiAfk.Pos = Pos
+			--ply.AntiAfk.Ang = Ang
+			ply.AntiAfk.LastChange = CurTime()
+			ply.AntiAfk.NotifShowed = nil
+			--print("PlayerPosChanged")
+		end
+		
+		if ply.AntiAfk.LastChange then
+			local Delta = CurTime() - ply.AntiAfk.LastChange
+			if not ply.AntiAfk.NotifShowed and AntiAfkTime - Delta < 60 then ply:ChatPrint("Вас кикнет менее чем через минуту, если вы не пошевелнётесь!") ply.AntiAfk.NotifShowed = true end
+			if Delta > AntiAfkTime then ply:Kick("AntiAfk") end
+		end
+	end
+	
+	AntiAfkTime = AntiAfkTime*2
+	if Metrostroi and Metrostroi.TrainClasses then
+		for k,TrainClass in pairs(Metrostroi.TrainClasses) do
+			for n,wag in pairs(ents.FindByClass(TrainClass)) do
+				if not IsValid(wag) then continue end
+				if not wag.AntiAfk then wag.AntiAfk = {} end
+				local Pos = wag:GetPos()
+				--local Ang = wag:GetAngles() --wag:EyeAngles()
+				if not wag.AntiAfk.Pos or wag.AntiAfk.Pos:DistToSqr(Pos) > 50*50
+				--or not wag.AntiAfk.Ang or wag.AntiAfk.Ang ~= Ang 
+				then
+					wag.AntiAfk.Pos = Pos
+					--wag.AntiAfk.Ang = Ang
+					wag.AntiAfk.LastChange = CurTime()
+					wag.AntiAfk.NotifShowed = nil
+					--print("PlayerPosChanged")
+				end
+				
+				if wag.AntiAfk.LastChange then
+					local Delta = CurTime() - wag.AntiAfk.LastChange
+					if not wag.AntiAfk.NotifShowed and AntiAfkTime - Delta < 60 and CPPI and IsValid(wag:CPPIGetOwner()) then
+						wag:CPPIGetOwner():ChatPrint("Ваш состав удлится менее чем через минуту, если он не сдвинется!") 
+						if wag.WagonList then
+							for i,wag1 in pairs(wag.WagonList) do
+								if not wag1.AntiAfk then wag1.AntiAfk = {} end
+								wag1.AntiAfk.NotifShowed = true
+							end
+						else
+							wag.AntiAfk.NotifShowed = true 
+						end
+					end
+				if Delta > AntiAfkTime then wag:Remove() end
+				end
+			end
+		end
+	end
+end)
+
