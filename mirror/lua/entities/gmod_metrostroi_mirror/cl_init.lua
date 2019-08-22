e796d72d46135b5ad898dde9e56e35930bbf9001
TheFulDeep <zlobindd@mail.ru>
2019-08-22 19:58:02 +0300

    littl echanges

--- mirror/lua/entities/gmod_metrostroi_mirror/cl_init.lua
+++ mirror/lua/entities/gmod_metrostroi_mirror/cl_init.lua
@@ -2,22 +2,27 @@ include("shared.lua")
 
 function ENT:Initialize()
 	self.RTCam = GetGlobalEntity("MirrorRTCam")
+	--self.RTCam:SetKeyValue("FOV",10)
 end
 
-function ENT:ShouldRenderClientEnts()
-end
+--[[function ENT:ShouldRenderClientEnts()
+end]]
 
 --[[function ENT:Think()
 
 end]]
-local MetrostroiDrawCams
+
+
+local MetrostroiDrawCams--,C_CabFOV,C_FovDesired
 timer.Simple(0,function()
 	MetrostroiDrawCams = GetConVar("metrostroi_drawcams")
+	--C_CabFOV              = GetConVar("metrostroi_cabfov")
+	--C_FovDesired          = GetConVar("fov_desired")
 end)
 
 function ENT:Draw()
 	if MetrostroiDrawCams and not MetrostroiDrawCams:GetBool() then return end
-	if not self:ShouldRenderClientEnts() then return end
+	--if not self:ShouldRenderClientEnts() then return end
 	if not IsValid(self.RTCam) then self.RTCam = GetGlobalEntity("MirrorRTCam") return end
 
 	self:DrawModel()
@@ -39,9 +44,22 @@ function ENT:Draw()
 	local MirrorPos = self:LocalToWorld(self:OBBCenter())
 	self.RTCam:SetPos(MirrorPos)
 	--if self.RTCam:GetPos():DistToSqr(MirrorPos) > 300*300 then return end
-	local plypos = LocalPlayer():EyePos()
+	local ply = LocalPlayer()
+	local plypos = ply:EyePos()
 	local ang = (plypos - MirrorPos):Angle()
 	ang = -ang + self:LocalToWorldAngles(self:GetAngles()) + Angle(0,-180,0)
 	ang:Normalize()
 	self.RTCam:SetAngles(ang)
+	
+	--[[local FOV
+	local Dist = plypos:DistToSqr(MirrorPos)
+	local veh = ply:GetVehicle()
+	if IsValid(veh) then
+		veh = veh:GetNW2Entity("TrainEntity")
+	end
+	if IsValid(veh) then
+		FOV = C_CabFOV:GetFloat()
+	else
+		FOV = C_FovDesired:GetFloat()
+	end]]
 end
