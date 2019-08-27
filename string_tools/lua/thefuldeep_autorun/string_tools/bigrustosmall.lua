636ab722b0dd7e52e88b18f8f919265cec8bdb7f
TheFulDeep <zlobindd@mail.ru>
2019-08-27 14:10:24 +0300

    bigrustosmall faster

--- string_tools/lua/thefuldeep_autorun/string_tools/bigrustosmall.lua
+++ string_tools/lua/thefuldeep_autorun/string_tools/bigrustosmall.lua
@@ -1,20 +1,10 @@
 --[[============================= string.lower ДЛЯ РУССКИХ СИМВОЛОВ ==========================]]
 local BIGRUS = {"А","Б","В","Г","Д","Е","Ё","Ж","З","И","Й","К","Л","М","Н","О","П","Р","С","Т","У","Ф","Х","Ц","Ч","Ш","Щ","Ъ","Ы","Ь","Э","Ю","Я"}
 local smallrus = {"а","б","в","г","д","е","ё","ж","з","и","й","к","л","м","н","о","п","р","с","т","у","ф","х","ц","ч","ш","щ","ъ","ы","ь","э","ю","я"}
-local BIG_to_small = {}
-for k, v in next, BIGRUS do
-   
-	BIG_to_small[v] = smallrus[k]
-   
-end
+
 function bigrustosmall(str)
-   
-	local strlow = ""
-   
-	for v in string.gmatch(str, "[%z\x01-\x7F\xC2-\xF4][\x80-\xBF]*") do
-		strlow = strlow .. (BIG_to_small[v] or v)
+	for i,letter in pairs(BIGRUS) do
+		str = str:gsub(BIGRUS[i],smallrus[i])
 	end
-   
-	return string.lower(strlow) --жтобы англ буквы тоже занижалис
-   
+	return string.lower(str)
 end
