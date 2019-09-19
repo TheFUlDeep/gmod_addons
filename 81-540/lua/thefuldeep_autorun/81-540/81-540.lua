7b34e6befc39f2210a4ccb9140778b6ef029d931
TheFulDeep <zlobindd@mail.ru>
2019-09-19 15:39:08 +0300

    81-540 almost done

--- 81-540/lua/thefuldeep_autorun/81-540/81-540.lua
+++ 81-540/lua/thefuldeep_autorun/81-540/81-540.lua
@@ -1,19 +1,26 @@
---[[if SERVER then		--пока что не стоит
-	resource.AddWorkshop("1768332944")
-end]]
+local Class = "gmod_subway_81-717_lvz"
 
+if SERVER then
+	resource.AddWorkshop("1768332944")
+	timer.Create("UpdateNWFor 540model",1,0,function()
+		for _,ent in pairs(ents.FindByClass(Class)) do
+			if ent:GetNW2Bool("81-540model") then
+				ent:SetNW2Bool("KVR",true)
+				--ent:SetNW2Int("AVType",1)
+			end
+		end
+	end)
+end
 
 --взято из скрипта межвагонки ShadowBonnie
 timer.Simple(0,function()
 	--for k,v in pairs(Metrostroi.TrainClasses) do
-		local ENT = scripted_ents.GetStored("gmod_subway_81-717_lvz_custom")
+		local ENT = scripted_ents.GetStored(Class)
 		if ENT and ENT.t and ENT.t.Spawner then
 			local cont = false
 			for k,v in pairs(ENT.t.Spawner) do
-				if istable(v) and v[1]=="81-540model" then cont = true break end
+				if istable(v) and v[1]=="81-540model" then return end
 			end
-			if cont then continue end
-		
 			table.insert(ENT.t.Spawner,{"81-540model","Кузов 81-540","Boolean"})
 		end
 	--end
@@ -21,23 +28,76 @@ end)
 
 if SERVER then return end
 
-local function ChangeModel(ent)
-	local Class = ent:GetClass()
-	if Class ~= "gmod_subway_81-717_lvz_custom" then return end
-	if not ent:GetNW2Bool("81-540model") then return end
-	ent:SetModel("models/metrostroi_train/81-5402/81-5402_body.mdl")
-	--TODO клиентпропы (+ их сдвиг, если понадобится)
+local function SetPrevModel(ent)
+	ent.model540.Preved = true
 	
-	
-	--обновление модели при нажатии спавнером на вагон
-	if ent.TrainSpawnerUpdate and not ent.TrainSpawnerUpdate540modelchanched then
-		ent.TrainSpawnerUpdate540modelchanched = true
-		local OldTrainSpawnerUpdate = ent.TrainSpawnerUpdate
-		ent.TrainSpawnerUpdate = function(ent)
-			OldTrainSpawnerUpdate(ent)
-			ChangeModel(ent)
+	local TrainModel = ent:GetModel()
+	if ent.model540.PrevModel and TrainModel ~= ent.model540.PrevModel then ent:SetModel(ent.model540.PrevModel) end
+	for k,v in pairs(ent.ClientEnts) do
+		if not IsValid(v) then continue end
+		if k == "mask22_1" or k == "mask222_lvz" or k == "mask22_2" and v:GetModelScale() ~= 1 then
+			v:SetModelScale(1)
+		end
+		if k == "cabine_old" or k == "cabine_new" or k == "RedLights" or k == "Headlights222_1" or k == "Headlights22_1" or k == "Headlights222_2" or k == "Headlights22_2" or k == "Lamps_cab2" or k == "Lamps_cab1" then --внутренняя кабина
+			local EntModel = v:GetModel()
+			if ent.model540[k] and EntModel ~= ent.model540[k] then v:SetModel(ent.model540[k]) end
 		end
 	end
 end
 
-hook.Add( "OnEntityCreated", "81-540model", ChangeModel)--TODO не уверен, что триггернетися на все ентити в клиентской части, и будет ли это триггериться, если ентити уже есть, а игрок только заходит на сервер
+timer.Create("UpdateClientEnts on 540",1,0,function()
+	for _,ent in pairs(ents.FindByClass(Class)) do
+		if not IsValid(ent) then continue end
+		if not ent.model540 then ent.model540 = {} end
+		if not ent:GetNW2Bool("81-540model") then 
+			if not ent.model540.Preved then SetPrevModel(ent) end
+			return 
+		end
+		
+		ent.model540.Preved = false
+		
+		local TrainModel = ent:GetModel()
+		if TrainModel ~= "models/metrostroi_train/81-5402/81-5402_body.mdl" then 
+			ent.model540.PrevModel = TrainModel 
+			ent:SetModel("models/metrostroi_train/81-5402/81-5402_body.mdl")
+		end
+		
+		if not ent.ClientEnts then return end
+		for k,v in pairs(ent.ClientEnts) do
+			if not IsValid(v) then continue end
+			local EntModel = v:GetModel()
+			if (k == "cabine_old" or k == "cabine_new") and EntModel ~= "models/metrostroi_train/81-5402/81-5402_cab.mdl" then --внутренняя кабина
+				ent.model540[k] = EntModel
+				v:SetModel("models/metrostroi_train/81-5402/81-5402_cab.mdl")
+			end
+			if k == "mask22_1" or k == "mask222_lvz" or k == "mask22_2" then
+				v:SetModelScale(0)
+			end
+			if k == "RedLights" and EntModel ~= "models/metrostroi_train/81-5402/redlights.mdl" then
+				ent.model540[k] = EntModel
+				v:SetModel("models/metrostroi_train/81-5402/redlights.mdl")
+			end
+			if (k == "Headlights222_1" or k == "Headlights22_1") and EntModel ~= "models/metrostroi_train/81-5402/group1.mdl" then
+				ent.model540[k] = EntModel
+				v:SetModel("models/metrostroi_train/81-5402/group1.mdl")
+			end
+			if (k == "Headlights222_2" or k == "Headlights22_2") and EntModel ~= "models/metrostroi_train/81-5402/group2.mdl" then
+				ent.model540[k] = EntModel
+				v:SetModel("models/metrostroi_train/81-5402/group2.mdl")
+			end
+			if (k == "Headlights222_2" or k == "Headlights22_2") and EntModel ~= "models/metrostroi_train/81-5402/group2.mdl" then
+				ent.model540[k] = EntModel
+				v:SetModel("models/metrostroi_train/81-5402/group2.mdl")
+			end
+			if (k == "Lamps_cab1" or k == "Lamps2_cab1") and EntModel ~= "models/metrostroi_train/81-5402/lamp_cab1.mdl" then
+				ent.model540[k] = EntModel
+				v:SetModel("models/metrostroi_train/81-5402/lamp_cab1.mdl")
+			end
+			if (k == "Lamps_cab2" or k == "Lamps2_cab2") and EntModel ~= "models/metrostroi_train/81-5402/lamp_cab2.mdl" then --TODO вторая ламп не вешается
+				ent.model540[k] = EntModel
+				v:SetModel("models/metrostroi_train/81-5402/lamp_cab2.mdl")
+			end
+		end
+	end
+end)
+
