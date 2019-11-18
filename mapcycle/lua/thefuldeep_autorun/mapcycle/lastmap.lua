822125c7d76a9c462a1487a462052f7bcfbeb110
TheFulDeep <zlobindd@mail.ru>
2019-11-18 15:27:45 +0300

    fix

--- mapcycle/lua/thefuldeep_autorun/mapcycle/lastmap.lua
+++ mapcycle/lua/thefuldeep_autorun/mapcycle/lastmap.lua
@@ -4,7 +4,7 @@ timer.Create("CheckLastMap",1,0,function()
 	if not THEFULDEEP or not THEFULDEEP.MAP then return end
 	if file.Exists( "lastmap.txt", "DATA" ) then
 		local lastmap = file.Read( "lastmap.txt", "DATA" )
-		if lastmap and THEFULDEEP.MAP ~= lastmap then 
+		if lastmap and lastmap:lower():find("%a") and THEFULDEEP.MAP ~= lastmap then 
 			game.ConsoleCommand("changelevel "..lastmap.."\n") 		
 			return
 		end
