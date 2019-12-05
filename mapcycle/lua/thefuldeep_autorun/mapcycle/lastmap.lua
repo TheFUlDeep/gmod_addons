53e1cc008d46be616ea2fa691a0a6706650a6a7a
TheFulDeep <zlobindd@mail.ru>
2019-12-05 11:39:56 +0300

    lastmap delay tryfix

--- mapcycle/lua/thefuldeep_autorun/mapcycle/lastmap.lua
+++ mapcycle/lua/thefuldeep_autorun/mapcycle/lastmap.lua
@@ -1,6 +1,6 @@
 --[[============================= ЗАГРУЗКА ПОСЛЕДНЕЙ КАРТЫ сохранение последней карты прописано в mapcycle ==========================]]
 if SERVER then
-timer.Simple(0,function()
+timer.Simple(3,function()--3, чтобы человек мог получить соединение при смене карты
 	if file.Exists( "lastmap.txt", "DATA" ) then
 		local lastmap = file.Read( "lastmap.txt", "DATA" )
 		if lastmap and lastmap:lower():find("%a") and lastmap ~= game.GetMap() then 
