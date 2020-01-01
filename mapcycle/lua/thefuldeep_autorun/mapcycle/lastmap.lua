41c38cd44992f41cc269cad516f9177853836737
thefuldeep <zlobindd@mail.ru>
2020-01-01 18:59:36 +0300

    lastmap and little fix

--- mapcycle/lua/thefuldeep_autorun/mapcycle/lastmap.lua
+++ mapcycle/lua/thefuldeep_autorun/mapcycle/lastmap.lua
@@ -4,7 +4,7 @@ timer.Simple(3,function()--3, чтобы человек мог получить 
 	if file.Exists( "lastmap.txt", "DATA" ) then
 		local lastmap = file.Read( "lastmap.txt", "DATA" )
 		if lastmap and lastmap:lower():find("%a") and lastmap ~= game.GetMap() then 
-			--game.ConsoleCommand("changelevel "..lastmap.."\n") 		
+			game.ConsoleCommand("changelevel "..lastmap.."\n") 		
 		end
 	end
 end)
