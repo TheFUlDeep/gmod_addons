39c52a44fbb92ab259295636c4ff5bf0dd772328
thefuldeep <zlobindd@mail.ru>
2020-04-05 22:07:21 +0300

    for sure

--- custom_commands/lua/thefuldeep_autorun/server_autorestart.lua
+++ custom_commands/lua/thefuldeep_autorun/server_autorestart.lua
@@ -2,5 +2,5 @@ if CLIENT then return end
 
 local serverstarted = os.time()
 timer.Create("autorestart",10,0,function()
-	if os.time() - serverstarted > 6*60*60 and table.IsEmpty(player.GetHumans()) then RunConsoleCommand("_restart") end
+	if ((os.time() - serverstarted) > (6*60*60)) and table.IsEmpty(player.GetHumans() or {}) then RunConsoleCommand("_restart") end
 end
