e999bbe48c5029b874e2359e56235fc61d8464cf
TheFUlDeep <zlobindd@mail.ru>
2020-07-05 20:28:47 +0300

    hotfix

--- metrostroi_scripts/lua/thefuldeep_autorun/metrostroi_scripts/717_route_number_from_asnp.lua
+++ metrostroi_scripts/lua/thefuldeep_autorun/metrostroi_scripts/717_route_number_from_asnp.lua
@@ -1,7 +1,7 @@
 --TODO правильнее было бы ворваться в систему ASNP и там обновлять сетевые значения при нажатии на кнопку
 if CLIENT then return end
 timer.Create("set routenumber by asnp for 81-717",5,0,function()
-	for _,ent in pairs(ents.FindByClass(nomerogg))do
+	for _,ent in pairs(ents.FindByClass("gmod_subway_81-717_mvm"))do
 		if not IsValid(ent)then return end
 		ent:SetNW2String("RouteNumber",ent:GetNW2Int("ASNP:RouteNumber",0)*10)
 	end
