235bba4fc4e4151cd26d7beda9f9b9eed5f47f39
TheFUlDeep <zlobindd@mail.ru>
2020-08-29 15:00:08 +0300

    hidetrains now fearching angles, upgraded mirrors

--- mirror/lua/thefuldeep_autorun/gmod_metrostroi_mirror.lua
+++ mirror/lua/thefuldeep_autorun/gmod_metrostroi_mirror.lua
@@ -209,16 +209,68 @@ THEFULDEEP.MirrorDraw = function(self)
 end
 
 
+
+local function IsPointOnSection(point,Start,End,skipLineChecking)
+	if not skipLineChecking then
+		--проверка, что точка на той же линии
+		if (End-Start):GetNormalized() ~= (point-Start):GetNormalized() then return end
+	end
+	
+	local x,y,z = point[1],point[2],point[3]
+	local sx,sy,sz = Start[1],Start[2],Start[3]
+	local ex,ey,ez = End[1],End[2],End[3]
+	--проверка, что точка в пределах отрезка
+	if (x >= sx and x <= ex or x >= ex and x <= sx) and (y >= sy and y <= ey or y >= ey and y <= sy) and (z >= sz and z <= ez or z >= ez and z <= sz) then return true end
+end
+
+local function IsPointInEntity(ent,vec)
+	local vec = ent:WorldToLocal(vec)
+	local x,y,z = vec[1],vec[2],vec[3]
+	
+	local mins = ent:OBBMins()+Vector(-100,-100,-100)
+	local xmin,ymin,zmin = mins[1],mins[2],mins[3]
+	
+	local maxs = ent:OBBMaxs()+Vector(100,100,100)
+	local xmax,ymax,zmax = maxs[1],maxs[2],maxs[3]
+	
+	return x <= xmax and x >= xmins and y <= ymax and y >= ymins and z <= zmax and z >= zmins
+end
+
+
+local AnglesForPlanes = {Angle(0,0,0),Angle(90,0,0),Angle(0,90,0)}
+local function IsSectionPartInEntity(ent,Start,End)
+	if IsPointInEntity(ent,Start) or IsPointInEntity(ent,End) then return true end
+	
+	print("asd")
+	
+	local dir = (End-Start)--:GetNormalized()
+	
+	local entang = ent:GetAngles()
+	
+	--первое число - вверх - -90, вниз - 90
+	--второе число - вправо - -90, влево - 90
+	for i = 1,2 do
+		local planepos = i == 1 and ent:LocalToWorld(ent:OBBMins()+Vector(-100,-100,-100)) or ent:LocalToWorld(ent:OBBMaxs()+Vector(100,100,100))
+		for k = 1,3 do
+			local Intersect = util.IntersectRayWithPlane(Start, dir, planepos, (entang + AnglesForPlanes[k]):Forward())
+			if Intersect and IsPointInEntity(ent,Intersect) then return true end
+		end
+	end
+end
+
 timer.Simple(0,function()--TODO надо, чтобы оно пропускало ентити паравозаф
-	local IfSee = Metrostroi.ShouldHideTrain
-	if not IfSee then return end
 	timer.Create("check if player can see mirror",1,0,function()
+		local ply = LocalPlayer and LocalPlayer()
+		if not IsValid(ply) then return end
 		for _,ent in pairs(ents.FindByClass("gmod_metrostroi_mirror")) do
 			if not IsValid(ent) then return end
-			ent.ShouldDraw = IfSee(ent)
+			local trace = ply:GetEyeTraceNoCursor()
+			ent.ShouldDraw = IsSectionPartInEntity(ent,trace.StartPos,trace.HitPos)
 		end
 	end)
 end)
+
+print("asd0")
 
 local MirrorPreview = CreateClientConVar("mirror_preview", "0", false)
 local MirrorDistance = CreateClientConVar("mirror_distance", "50", false)
@@ -229,6 +281,8 @@ local MirrorScale = CreateClientConVar("mirror_scale", "1", false)
 local MirrorModel = CreateClientConVar("mirror_model_type", "1", false)
 local MirrorPreviewHotkey = CreateClientConVar("mirror_preview_hotkey", "", true)
 local MirrorSrickDown = CreateClientConVar("mirror_stick_down", "0", false)
+
+print("asd1")
 
 if not game.SinglePlayer() then
 	hook.Add("PlayerButtonDown","Mirror_preview_toggle",function(ply, button )
