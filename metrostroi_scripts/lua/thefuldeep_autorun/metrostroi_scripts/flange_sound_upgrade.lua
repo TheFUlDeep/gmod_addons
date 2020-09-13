760c3adc8214fac18a8784388a2f96e8f5427be3
TheFUlDeep <zlobindd@mail.ru>
2020-09-13 17:04:38 +0300

    tab

--- metrostroi_scripts/lua/thefuldeep_autorun/metrostroi_scripts/flange_sound_upgrade.lua
+++ metrostroi_scripts/lua/thefuldeep_autorun/metrostroi_scripts/flange_sound_upgrade.lua
@@ -314,7 +314,7 @@ timer.Simple(1,function()
 		for ent,sounds in pairs(Sounds)do
 			if not IsValid(ent) or ent:IsDormant()then
 				for _,snd in pairs(sounds or {})do
-			       if snd:IsPlaying() then
+					if snd:IsPlaying() then
 						print("stopping unlegit sound")
 						snd:ChangeVolume(0.0,0)
 						snd:Stop()
