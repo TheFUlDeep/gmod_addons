e3a9037cceaf617a5aaa9d48e6eaaf4bacf34506
TheFUlDeep <zlobindd@mail.ru>
2020-09-15 20:55:35 +0300

    fixes

--- custom_commands/lua/thefuldeep_autorun/some_props_collision/some_props_collision_disabling.lua
+++ custom_commands/lua/thefuldeep_autorun/some_props_collision/some_props_collision_disabling.lua
@@ -2,10 +2,16 @@ if CLIENT then return end
 
 local Ents = {"streamradio","camera","mediaplayer"}
 
-hook.Add("OnEntityCreated","Disabling collisions on some props for metrostroi",function(ent)
+local function func(ent)
 	if not IsValid(ent) then return end
 	local Class = ent:GetClass():lower()
 	for _,v in pairs(Ents) do
-		if Class:find(v,1,true) then ent:SetCollisionGroup(COLLISION_GROUP_WORLD) return end
+		if Class:find(v,1,true) then ent:SetCollisionGroup(COLLISION_GROUP_INTERACTIVE_DEBRIS)return end
 	end
+end
+
+hook.Add("OnEntityCreated","Disabling collisions on some props for metrostroi",function(ent)
+	--делаю два раза, потому что с одного раза почему-то для камеры не ставится нужная коллизия
+	func(ent)
+	timer.Simple(0,function()func(ent)end)
 end)
