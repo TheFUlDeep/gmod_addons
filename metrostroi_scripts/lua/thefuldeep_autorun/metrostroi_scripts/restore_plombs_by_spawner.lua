844ec734bdd1b0ae08b4a531bc1f1c8b2698092d
TheFUlDeep <zlobindd@mail.ru>
2020-09-15 14:47:43 +0300

    restoring blombs by spawner

---
+++ metrostroi_scripts/lua/thefuldeep_autorun/metrostroi_scripts/restore_plombs_by_spawner.lua
@@ -0,0 +1,36 @@
+if CLIENT then return end
+
+timer.Simple(1,function()
+	local function RestoreBlomb(self,but,val)
+		self[but]:TriggerInput("Set",val)
+		self[but]:TriggerInput("Block",true)
+		self.Plombs[but] = true
+		self:SetPackedBool(but.."Pl",true)
+	end
+	
+
+	local BrokenPlombs = {}
+	
+	local BASE = scripted_ents.GetStored("gmod_subway_base")
+	if not BASE then return else BASE = BASE.t end
+	
+	local oldbroke = BASE.BrokePlomb
+	BASE.BrokePlomb = function(self,but,...)
+		oldbroke(self,but,...)
+		BrokenPlombs[self] = BrokenPlombs[self] or {}
+		BrokenPlombs[self][but] = self[but].Value
+	end
+	
+	
+	for _,class in pairs(Metrostroi and Metrostroi.TrainClasses or {})do
+		local ENT = scripted_ents.GetStored(class)
+		if not ENT then continue else ENT = ENT.t end
+		local oldupdate = ENT.TrainSpawnerUpdate or function()end
+		ENT.TrainSpawnerUpdate = function(self,...)
+			oldupdate(self,...)
+			for but,val in pairs(BrokenPlombs[self] or {})do
+				RestoreBlomb(self,but,val)
+			end
+		end
+	end
+end)
