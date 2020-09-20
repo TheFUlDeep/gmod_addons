774b6b71e81cfcfd8f2545780b76e0935385cba2
TheFUlDeep <zlobindd@mail.ru>
2020-09-20 17:03:48 +0300

    fists script

--- metrostroi_scripts/lua/thefuldeep_autorun/metrostroi_scripts/fists_can_attack_only_couples.lua
+++ metrostroi_scripts/lua/thefuldeep_autorun/metrostroi_scripts/fists_can_attack_only_couples.lua
@@ -3,23 +3,21 @@ if CLIENT then return end
 
 local function TraceCouple(owner)
     if not IsValid(owner) then return end
-    local ent = owner:GetEyeTraceNoCursor().Entity
-    if IsValid(ent) and ent:GetClass() == "gmod_train_couple" then return true end
+	local trace = owner:GetEyeTraceNoCursor()
+    local ent = trace.Entity
+    if IsValid(ent) and ent:GetClass() == "gmod_train_couple" then return ent, trace.HitPos end
 end
 
-timer.Simple(0,function()
+hook.Add("InitPostEntity","upgrade fists for metrostroi",function()
     local weapon = weapons.GetStored("weapon_fists")
     if not weapon then return end
     print("upgrading fists...")
-    for i = 1,2 do
-        local attack = i == 1 and "PrimaryAttack" or "SecondaryAttack"
-        local oldattack = weapon[attack] or function()end
-        weapon[attack] = function(self,...)        
-            local ostime = os.time()
-            if not self.LastAttackTimesTamp or ostime - self.LastAttackTimesTamp > 0 then self.LastAttackTimesTamp = ostime else return end
-            if not TraceCouple(self.Owner) then return end
-            return oldattack(self,...)
-        end
-    end
+	local distlimit = 50*50
+	local oldDealdmg = weapon.DealDamage or function()end
+	weapon.DealDamage = function(self,...)
+		local couple,hitpos = TraceCouple(self.Owner)
+		if not couple or couple and self.Owner:GetShootPos():DistToSqr(hitpos) > distlimit then return end
+		oldDealdmg(self,...)
+	end
 end)
 
