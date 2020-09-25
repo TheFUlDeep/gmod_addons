13be88ef35fcbf367edc9b8015c04f30f7346ac3
TheFUlDeep <zlobindd@mail.ru>
2020-09-25 08:47:33 +0300

    commants update

--- metrostroi_scripts/lua/thefuldeep_autorun/metrostroi_scripts/als_upgrade.lua
+++ metrostroi_scripts/lua/thefuldeep_autorun/metrostroi_scripts/als_upgrade.lua
@@ -1,11 +1,4 @@
 if CLIENT then return end
---TODO
---[[
-	GetARSJoint принимает train и передает его в ARSJointScan и ARSJointScanBack.
-	А эти функции для опредедления позиции обращаются к Metrostroi.TrainPositions[train]
-	А эта таблица заполняется в методе UpdateTrainPositions, и там берутся позиции вагонов через GetPos(), то есть их центр.
-	Но правильнее же брать позицию тележки, так как именно там находится принимающая катушка
-]]
 timer.Simple(0,function()
 	--первый раз ищется стандартно
 	--Если не нашел, то ищется по тому же треку в радиусе 350. Если нашел, то проверяется, что состав точно на этом треке
@@ -39,11 +32,34 @@ timer.Simple(0,function()
 		end
 	end
 
-	Metrostroi.GetARSJoint = function(node,x,dir,train)
-		local forw,back = oldGetARSJoint(node,x,dir,train)
+	Metrostroi.GetARSJoint = function(node,x,...)
+		local forw,back = oldGetARSJoint(node,x,...)
 		if train and not forw then
 			forw = CheckForw(train,nil,node)
 		end
 		return forw,back
 	end
+	
+	
+	--мем
+	--[[
+		GetARSJoint принимает train и передает его в ARSJointScan и ARSJointScanBack.
+		А эти функции для опредедления позиции обращаются к Metrostroi.TrainPositions[train]
+		А эта таблица заполняется в методе UpdateTrainPositions, и там берутся позиции вагонов через GetPos(), то есть их центр.
+		Но правильнее же брать позицию тележки, так как именно там находится принимающая катушка
+		Но при проверке оказалось, что в игре частота меняется при проезде тележкой, то есть оно работает правильно. Забавно
+	]]
+	--[[local oldupdate = Metrostroi.UpdateTrainPositions
+	Metrostroi.UpdateTrainPositions = function(...)
+		oldupdate(...)
+		for train in pairs(Metrostroi.TrainPositions) do
+			if IsValid(train) and IsValid(train.FrontBogey) then
+				local pos = Metrostroi.GetPositionOnTrack(train.FrontBogey:GetPos()),train.FrontBogey:GetAngles())[1]
+				if pos then
+					Metrostroi.TrainPositions[train] = {}
+					Metrostroi.TrainPositions[train][1] = pos
+				end
+			end
+		end
+	end]]
 end)
