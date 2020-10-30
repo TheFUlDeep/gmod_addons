fa4322c7ef7c48478c60ff1083d6ea1ba22aaf7d
TheFUlDeep <zlobindd@mail.ru>
2020-10-30 23:19:21 +0300

    new addons blanks

---
+++ metrostroi_scripts/lua/thefuldeep_autorun/metrostroi_scripts/717_mask_262.lua
@@ -0,0 +1,82 @@
+if SERVER then resource.AddWorkshop("2018098142") end
+
+if CLIENT then
+	MetrostroiWagNumUpdateRecieve = MetrostroiWagNumUpdateRecieve or function(index)
+		local ent = Entity(index)
+		--таймер, чтобы дождаться обновления сетевых значений (ну а вдруг)
+		timer.Simple(0.3,function()
+			if IsValid(ent) and ent.UpdateWagNumCallBack then 
+				ent:UpdateWagNumCallBack()
+				--ent:UpdateTextures()
+			end
+		end)
+	end
+end
+
+if SERVER then
+	local hooks = hook.GetTable()
+	if not hooks.MetrostroiSpawnerUpdate or not hooks.MetrostroiSpawnerUpdate["Call hook on clientside"] then
+		hook.Add("MetrostroiSpawnerUpdate","Call hook on clientside",function(ent)
+			if not IsValid(ent) then return end
+			local idx = ent:EntIndex()
+			for _,ply in pairs(player.GetHumans())do
+				if IsValid(ply)then ply:SendLua("MetrostroiWagNumUpdateRecieve("..idx..")")end
+			end
+		end)
+	end
+end
+
+local function RemoveEnt(wag,prop)
+	local ent = wag.ClientEnts and wag.ClientEnts[prop]
+	if IsValid(ent) then SafeRemoveEntity(ent)end
+end
+
+local function UpdateCpropCallBack(ENT,cprop,modelcallback,precallback,callback)	
+	if not ENT.UpdateWagNumCallBack then
+		function ENT:UpdateWagNumCallBack()end
+	end
+	
+	if modelcallback then
+		local oldmodelcallback = ENT.ClientProps[cprop].modelcallback or function() end
+		ENT.ClientProps[cprop].modelcallback = function(wag,...)
+			return modelcallback(wag) or oldmodelcallback(wag,...)
+		end
+		
+		local oldstartedcallback = ENT.UpdateWagNumCallBack
+		ENT.UpdateWagNumCallBack = function(wag)
+			oldstartedcallback(wag)
+			RemoveEnt(wag,cprop)
+		end
+	end
+	
+	if precallback then
+		local oldcallback = ENT.ClientProps[cprop].callback or function() end
+		ENT.ClientProps[cprop].callback = function(wag,cent,...)
+			precallback(wag,cent)
+			oldcallback(wag,cent,...)
+		end
+		
+		local oldstartedcallback = ENT.UpdateWagNumCallBack
+		ENT.UpdateWagNumCallBack = function(wag)
+			oldstartedcallback(wag)
+			RemoveEnt(wag,cprop)
+		end
+	end
+	
+	if callback then
+		local oldcallback = ENT.ClientProps[cprop].callback or function() end
+		ENT.ClientProps[cprop].callback = function(wag,cent,...)
+			oldcallback(wag,cent,...)
+			callback(wag,cent)
+		end
+		
+		local oldstartedcallback = ENT.UpdateWagNumCallBack
+		ENT.UpdateWagNumCallBack = function(wag)
+			oldstartedcallback(wag)
+			RemoveEnt(wag,cprop)
+		end
+	end
+end
+
+
+--TODO
