873ed2de2bff84868d6bf10613c96262b2b51695
TheFUlDeep <zlobindd@mail.ru>
2021-03-04 12:29:37 +0300

    disabled broken code

--- metrostroi_scripts/lua/metrostroi/sv_als_upgrade.lua
+++ metrostroi_scripts/lua/metrostroi/sv_als_upgrade.lua
@@ -383,46 +383,47 @@ hook.Add("MetrostroiLoaded","UpgradeTracks",function()
 	
 	
 	--TODO не проверено достаточно качественно
-	local function Rotate180(ang)if ang <= 180 then return ang + 180 else return ang - 180 end end
+	--на калинине из-за этого прикола ловятся рельсовые цепи с соседних треков и получается неправильная частота
+	-- local function Rotate180(ang)if ang <= 180 then return ang + 180 else return ang - 180 end end
 
-	--данный апгрейд сделан, чтобы не ловвить неправильный трек на перекрестиях
-	local oldGetPositionOnTrack = Metrostroi.GetPositionOnTrack
-	Metrostroi.GetPositionOnTrack = function(pos,ang,opts)
-		if not ang then
-			return oldGetPositionOnTrack(pos,ang,opts)
-		else
-			local res = oldGetPositionOnTrack(pos,ang,opts)
+	-- --данный апгрейд сделан, чтобы не ловвить неправильный трек на перекрестиях
+	-- local oldGetPositionOnTrack = Metrostroi.GetPositionOnTrack
+	-- Metrostroi.GetPositionOnTrack = function(pos,ang,opts)
+		-- if not ang then
+			-- return oldGetPositionOnTrack(pos,ang,opts)
+		-- else
+			-- local res = oldGetPositionOnTrack(pos,ang,opts)
 			
-			--если найдено хоть что-то
-			if res[1] then
-				ang = ang[2]
-				--метод :Angle возвращает угол от 0 до 360, а ang может прийти в формате от -180 до 180, поэтому явно преобразую в нужный мне формат
-				if ang < 0 then ang = -ang + 180 end
+			-- --если найдено хоть что-то
+			-- if res[1] then
+				-- ang = ang[2]
+				-- --метод :Angle возвращает угол от 0 до 360, а ang может прийти в формате от -180 до 180, поэтому явно преобразую в нужный мне формат
+				-- if ang < 0 then ang = -ang + 180 end
 				
-				--ищу трек, который лучше всего по углу подходит к искомому углу
-				local minang,minkey
-				for k,params in pairs(res)do
-					local node = params.node1
-					if node.next then
-						local curang = math.abs(ang - (node.pos - node.next.pos):Angle()[2])
-						curang = math.min(curang,Rotate180(curang))
-						if not minang or minang > curang then minang = curang minkey = k end
-					end
-					if node.prev then
-						local curang = math.abs(ang - (node.pos - node.prev.pos):Angle()[2])
-						curang = math.min(curang,Rotate180(curang))
-						if not minang or minang > curang then minang = curang minkey = k end
-					end
-				end
-				if minkey then
-					-- print("выбрал трек",res[minkey]..path.id)
-					res = {res[minkey]}
-				end
-			end
+				-- --ищу трек, который лучше всего по углу подходит к искомому углу
+				-- local minang,minkey
+				-- for k,params in pairs(res)do
+					-- local node = params.node1
+					-- if node.next then
+						-- local curang = math.abs(ang - (node.pos - node.next.pos):Angle()[2])
+						-- curang = math.min(curang,Rotate180(curang))
+						-- if not minang or minang > curang then minang = curang minkey = k end
+					-- end
+					-- if node.prev then
+						-- local curang = math.abs(ang - (node.pos - node.prev.pos):Angle()[2])
+						-- curang = math.min(curang,Rotate180(curang))
+						-- if not minang or minang > curang then minang = curang minkey = k end
+					-- end
+				-- end
+				-- if minkey then
+					-- -- print("выбрал трек",res[minkey]..path.id)
+					-- res = {res[minkey]}
+				-- end
+			-- end
 			
-			return res
-		end
-	end	
+			-- return res
+		-- end
+	-- end	
 end)
 
 
