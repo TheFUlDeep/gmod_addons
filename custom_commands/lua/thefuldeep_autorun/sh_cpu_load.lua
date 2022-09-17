8d1b8ac0cb8d220b9fd447e48317b312371113ca
TheFUlDeep <39480610+TheFUlDeep@users.noreply.github.com>
2022-09-17 15:08:53 +0300

    Create sh_cpu_load.lua

---
+++ custom_commands/lua/thefuldeep_autorun/sh_cpu_load.lua
@@ -0,0 +1,33 @@
+if SERVER then
+    require("cpu_info")
+    local count = GetProcessorsCount()
+    timer.Create("cpu_load",1,0,function()
+        local str = ""
+        for core = 1,count do
+            local load = GetProcessorLoad(core)
+            if load == -2147483648 then continue end
+            str = str..load.."\t"
+        end
+        SetGlobalString("cpu_load",str)
+    end)
+end
+if SERVER then return end
+
+
+--local c_developer = GetConVar("developer")
+--timer.Create("cpu_load",1,0,function()
+    --if not c_developer:GetBool() then return end
+    --print(GetGlobalString("cpu_load"))
+--end)
+
+local c_net_graph = GetConVar("net_graph")
+
+local white = Color(255,255,255)
+local textdata = {text = "",pos = {0,0}}
+local cpu_load_str = "cpu_load"
+local empty_str = ""
+hook.Add("HUDDrawScoreBoard", "cpu_load", function()
+    if not c_net_graph:GetBool() then return end
+    textdata.text = GetGlobalString(cpu_load_str,empty_str)
+    draw.Text(textdata)
+end)
