72ed8d22edcddcceef302cc7366b814451b28680
TheFUlDeep <39480610+TheFUlDeep@users.noreply.github.com>
2022-09-29 12:20:14 +0300

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,3 +1,16 @@
-for i = 1,2 do
-  if 1 then a = b continue end
+DIR_SEP="/" --should be "/" for Unix platforms (Linux and Mac)
+function browseFolder(root)
+	for entity in lfs.dir(root) do
+		if entity~="." and entity~=".." then
+			local fullPath=root..DIR_SEP..entity
+			local mode=lfs.attributes(fullPath,"mode")
+			if mode=="file" then
+				print(root.." > "..entity)
+			elseif mode=="directory" then
+				browseFolder(fullPath);
+			end
+		end
+	end
 end
+
+browseFolder(".")
